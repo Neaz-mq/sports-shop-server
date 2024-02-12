888f3c7ae3e504fc6aad74211f96468d88e288fe
Neaz-mq <neazmorshed666@gmail.com>
2024-02-12 15:47:18 +0600

    reviews updated

--- index.js
+++ index.js
@@ -27,9 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    
-
-    
+  
 
     const itemCollection = client.db("sportsDb").collection("item");
     const reviewCollection = client.db("sportsDb").collection("reviews");
