98bd49b011e79088849ae549c2201a47f97b3af5
Neaz-mq <neazmorshed666@gmail.com>
2024-02-27 10:59:00 +0600

    admin make

--- index.js
+++ index.js
@@ -51,8 +51,29 @@ async function run() {
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
+    });
+
+    app.patch('/users/admin/:id', async(req,res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const updatedDoc = {
+          $set: {
+            role: 'admin'
+          }
+        }
+        const result = await userCollection.updateOne(filter, updatedDoc)
+        res.send(result);
+
     })
 
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId()}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // Item related api
     app.get('/item', async (req, res) => {
       const result = await itemCollection.find().toArray();
       res.send(result);
